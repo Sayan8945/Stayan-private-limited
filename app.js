470f04f24d9cf65c136eea45782199a729cea40c
Sayan8945 <sayanpub2020@gmail.com>
2025-08-27 01:47:53 +0530

    latest

--- app.js
+++ app.js
@@ -31,12 +31,10 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.engine('ejs', engine);
 
 let MongoDbUrl = process.env.MONGO_DBLINK ;
-mongoose.connect(MongoDbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ MongoDB connected"))
-.catch(err => console.error("❌ MongoDB connection error:", err));
+main(() => console.log("connected with wanderlust")).catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(MongoDbUrl);
+}
 
 //mongo store
 const store = MongoStore.create({
